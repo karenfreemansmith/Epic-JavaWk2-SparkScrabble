687e78ecc6fd84dd2279db8c32970a0d43518c16
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-16 07:58:24 -0700

    add code and test for word

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,3 +1,11 @@
 public class Word {
+  private String mWord;
 
+  public Word(String text) {
+    mWord=text;
+  }
+
+  public String getWord() {
+    return mWord;
+  }
 }
--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -2,5 +2,15 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class WordTest {
+  @Test
+  public void Word_instantiatesCorrectly_true() {
+    Word myWord = new Word("dog");
+    assertEquals(true, myWord instanceof Word);
+  }
 
+  @Test
+  public void getWord_returnsValidText_String() {
+    Word myWord = new Word("dog");
+    assertEquals("dog", myWord.getWord());
+  }
 }
