6333a912b3da0e6725321c95cc86565e5b34fea6
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-16 07:54:36 -0700

    add code and test for definition

---
+++ src/main/java/Definition.java
@@ -0,0 +1,12 @@
+public class Definition {
+  private String mText;
+
+  public Definition(String text) {
+    mText=text;
+  }
+
+  public String getDefinition() {
+    return mText;
+  }
+
+}
--- src/test/java/DefinitionTest.java
+++ src/test/java/DefinitionTest.java
@@ -2,5 +2,16 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class DefinitionTest {
+  @Test
+  public void Definition_instantiatesCorrectly_true() {
+    Definition myDef = new Definition("an animal that barks");
+    assertEquals(true, myDef instanceof Definition);
+  }
+
+  @Test
+  public void getDefinition_returnsValidText_String() {
+    Definition myDef = new Definition("an animal that barks");
+    assertEquals("an animal that barks", myDef.getDefinition());
+  }
 
 }
