cf770805f323c179bb847384060249e5007ef1db
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-16 08:07:55 -0700

    add list of definitions to words

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,11 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Word {
   private String mWord;
+  private List<Definition> mDefinitions;
 
   public Word(String text) {
     mWord=text;
+    mDefinitions=new ArrayList<Definition>();
   }
 
   public String getWord() {
     return mWord;
+  }
+
+  public void addDefinition(Definition newDef) {
+    mDefinitions.add(newDef);
+  }
+
+  public List<Definition> getDefinitions() {
+    return mDefinitions;
   }
 }
--- src/test/java/WordTest.java
+++ src/test/java/WordTest.java
@@ -1,5 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class WordTest {
   @Test
@@ -12,5 +14,15 @@ public class WordTest {
   public void getWord_returnsValidText_String() {
     Word myWord = new Word("dog");
     assertEquals("dog", myWord.getWord());
+  }
+
+  @Test
+  public void addDefinition_addedToList_2() {
+    Word myWord = new Word("dog");
+    Definition myDef1 = new Definition("an animal that barks");
+    Definition myDef2 = new Definition("derogatory slang");
+    myWord.addDefinition(myDef1);
+    myWord.addDefinition(myDef2);
+    assertEquals(2, myWord.getDefinitions().size());
   }
 }
