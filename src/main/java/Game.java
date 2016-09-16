f76fb56be6b3a17a0654e9dc4994b64be45d064c
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-16 10:31:56 -0700

    add and test code for creating game tiles

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,3 +1,118 @@
 public class Game {
   private Square mSquares[][] = new Square[15][15];
+  private Tile mTiles[] = new Tile[100];
+
+  public Game() {
+    //setup squares on board
+    for(int i=0; i<=15; i++) {
+      for(int j=0; j<=15; j++) {
+        //mSquares[i][j] = new Square();
+      }
+    }
+    //set up tiles
+    int t = 0; //tracks total tiles
+    for(int i=1; i<=2; i++) {
+      mTiles[t] = new Tile(' ', 0);
+      t++;
+    }
+    for(int i=1; i<=12; i++) {
+      mTiles[t] = new Tile('E', 1);
+      t++;
+    }
+    for(int i=1; i<=9; i++) {
+      mTiles[t] = new Tile('A', 1);
+      t++;
+    }
+    for(int i=1; i<=9; i++) {
+      mTiles[t] = new Tile('I', 1);
+      t++;
+    }
+    for(int i=1; i<=8; i++) {
+      mTiles[t] = new Tile('O', 1);
+      t++;
+    }
+    for(int i=1; i<=6; i++) {
+      mTiles[t] = new Tile('N', 1);
+      t++;
+    }
+    for(int i=1; i<=6; i++) {
+      mTiles[t] = new Tile('R', 1);
+      t++;
+    }
+    for(int i=1; i<=6; i++) {
+      mTiles[t] = new Tile('T', 1);
+      t++;
+    }
+    for(int i=1; i<=4; i++) {
+      mTiles[t] = new Tile('L', 1);
+      t++;
+    }
+    for(int i=1; i<=4; i++) {
+      mTiles[t] = new Tile('S', 1);
+      t++;
+    }
+    for(int i=1; i<=4; i++) {
+      mTiles[t] = new Tile('U', 1);
+      t++;
+    }
+    for(int i=1; i<=4; i++) {
+      mTiles[t] = new Tile('D', 2);
+      t++;
+    }
+    for(int i=1; i<=3; i++) {
+      mTiles[t] = new Tile('G', 2);
+      t++;
+    }
+    for(int i=1; i<=2; i++) {
+      mTiles[t] = new Tile('B', 3);
+      t++;
+    }
+    for(int i=1; i<=2; i++) {
+      mTiles[t] = new Tile('C', 3);
+      t++;
+    }
+    for(int i=1; i<=2; i++) {
+      mTiles[t] = new Tile('M', 3);
+      t++;
+    }
+    for(int i=1; i<=2; i++) {
+      mTiles[t] = new Tile('P', 3);
+      t++;
+    }
+    for(int i=1; i<=2; i++) {
+      mTiles[t] = new Tile('F', 4);
+      t++;
+    }
+    for(int i=1; i<=2; i++) {
+      mTiles[t] = new Tile('H', 4);
+      t++;
+    }
+    for(int i=1; i<=2; i++) {
+      mTiles[t] = new Tile('V', 4);
+      t++;
+    }
+    for(int i=1; i<=2; i++) {
+      mTiles[t] = new Tile('W', 4);
+      t++;
+    }
+    for(int i=1; i<=2; i++) {
+      mTiles[t] = new Tile('Y', 4);
+      t++;
+    }
+    mTiles[t] = new Tile('K', 5);
+    t++;
+    mTiles[t] = new Tile('J', 8);
+    t++;
+    mTiles[t] = new Tile('X', 8);
+    t++;
+    mTiles[t] = new Tile('Q', 10);
+    t++;
+    mTiles[t] = new Tile('Z', 10);
+    t++;
+  }
+
+  public Tile getTile(int id) {
+    return mTiles[id-1];
+  }
+
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -8,4 +8,9 @@ public class GameTest {
   public void Game_instantiatesCorrectly_true() {
     assertEquals(true, myGame instanceof Game);
   }
+
+  @Test
+  public void Game_instantiates100TilesCorrectly_Z() {
+    assertEquals('Z', myGame.getTile(100).getMark());
+  }
 }
